5bc28079342ffe09eda0af78fe17d5c5d1c9fa4d
Dzheydaa <bakiceyda143@gmail.com>
2025-07-02 16:59:22 +0300

    class Data, class Constants

--- Data.cs
+++ Data.cs
@@ -6,8 +6,44 @@ using System.Threading.Tasks;
 
 namespace TicketSystemManagement
 {
+    using System.Text.Json;
+    using static Constants;
+
     public class Data
     {
+        public List<Event> Events { get; private set; }
 
+        private StreamReader reader;
+        private StreamWriter writer;
+
+        public Data()
+        {
+            LoadEvents();
+        }
+
+        public void Save()
+        {
+            StreamWriter writer = new StreamWriter(filePath);
+            using (writer)
+            {
+                string jsonData = JsonSerializer.Serialize(Events);
+                writer.Write(jsonData);
+            }
+        }
+
+        public void LoadEvents()
+        {
+           Events = new List<Event>();
+            reader = new StreamReader(filePath);
+            using (reader)
+            {
+                string jsonData = reader.ReadToEnd();
+                if (!string.IsNullOrEmpty(jsonData))
+                {
+                  Events = JsonSerializer.Deserialize<List<Event>>(jsonData)!;
+                }
+            }
+        }
     }
+
 }

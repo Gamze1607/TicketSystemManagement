986112bbbe34b2bb287994ab00bd4d7e00a14a1f
Gamze1607 <gami.ismail.07@gmail.com>
2025-07-02 18:58:25 +0300

    class Event

--- Event.cs
+++ Event.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace TicketSystemManagement
 {
-    internal class Event
+    public class Event
     {
+        public int eventId { get; set; }
+
+        public string name { get; set; }
+
+        public DateTime date { get; set; }
+
+        public string venue { get; set; }
+
+        public int totalTickets { get; set; }
+        
+        public int availableTickets { get; set; }
+        public decimal price { get; set; }
+
+        public Event(int id, string Name, DateTime Date, string Venue, int totaltickets, decimal Price)
+        {
+            eventId = id;
+            name = Name;
+            date = Date;
+            venue= Venue;
+            totalTickets = totaltickets;
+            availableTickets = totaltickets;
+            price = Price;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace TicketSystemManagement
 {
-    internal class Program
+    public class Program
     {
         public static void Main(string[] args)
         {

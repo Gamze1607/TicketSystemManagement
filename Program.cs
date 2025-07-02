5bee2291c10cd61f5dba019656e604c4bad12f79
User <User@DESKTOP-3ARVTMN>
2025-07-02 13:49:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketSystemManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Dzheyda!");
+        }
+    }
+}

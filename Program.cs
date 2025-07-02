f6ef4b4db41e59ee64b457226c99aefb58289ee8
Dzheydaa <bakiceyda143@gmail.com>
2025-07-02 16:32:26 +0300

    mg

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Dzheyda!");
+            Console.WriteLine("Hello, Dzheyda! zdr kak si");
+
 
 
         }

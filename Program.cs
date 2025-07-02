98f11cf144adb11411227e491c909078d9452f6b
Gamze1607 <gami.ismail.07@gmail.com>
2025-07-02 16:19:06 +0300

    demo try for push and pull

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello, Dzheyda!");
+
+
         }
     }
 }

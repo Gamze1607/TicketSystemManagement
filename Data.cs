24beaa2767c665f19ba05d0cb2eb42b0b41228e0
Dzheydaa <bakiceyda143@gmail.com>
2025-07-03 17:37:08 +0300

    class data and class constants

--- Data.cs
+++ Data.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,43 @@ using System.Threading.Tasks;
 
 namespace TicketSystemManagement
 {
+    using System.Text.Json;
+    using static Constants;
     public class Data
     {
+        public List<Event> Events { get; private set; }
+
+        private StreamReader reader;
+        private StreamWriter writer;
+
+        public Data()
+        {
+            LoadEvents();
+        }
+
+        public void Save()
+        {
+            StreamWriter writer = new StreamWriter(filePath);
+            using (writer)
+            {
+                string jsonData = JsonSerializer.Serialize(Events);
+                writer.Write(jsonData);
+            }
+        }
+
+        public void LoadEvents()
+        {
+            Events = new List<Event>();
+            reader = new StreamReader(filePath);
+            using (reader)
+            {
+                string jsonData = reader.ReadToEnd();
+                if (!string.IsNullOrEmpty(jsonData))
+                {
+                    Events = JsonSerializer.Deserialize<List<Event>>(jsonData)!;
+                }
+            }
+        }
 
     }
 }
